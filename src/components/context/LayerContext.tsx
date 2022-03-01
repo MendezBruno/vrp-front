8c56f2f4d63475ebf347cf93e260da8cfb60fdda
brunoCamOnApp <brunomendez88@hotmail.com>
2022-03-01 19:29:14 -0300

    update Action menu

--- src/components/context/LayerContext.tsx
+++ src/components/context/LayerContext.tsx
@@ -1,5 +1,7 @@
+import axios from 'axios';
 import { LatLng } from 'leaflet';
 import React, { useState } from 'react';
+import { Package } from '../../models/Package';
 
 export interface ILayerContex {
     points: any[],
@@ -16,9 +18,34 @@ const LayerContext = React.createContext<ILayerContex>(defaultContex);
 const LayerContextProvider = ({ children }: any) => {
     const defaultLatLng: any[] =  [[48.865572, 2.283523]];
     const [points, setPoint] = useState<any>(defaultLatLng);
+    const [route, setRoute] = useState<any>(null);
     const addPoint = (point: any) => { 
         setPoint([points, ...[point]])
         console.log(points);
+    }
+    const createRoute = async (points: any[]) => {
+        const aPackages: Package[] = []
+        points.forEach( aPoint => {
+            aPackages.push({ packageId: Math.random()*10000, location: aPoint  })
+        })
+        const start: Package | undefined = aPackages.shift()
+        const packageDTO = {
+            transactionId: (Math.random()*10000).toString(),
+            start: start,
+            packages: aPackages
+        }
+        try {
+            const { data } = await axios.post('http://localhost:5000/orsm/v2/trip', packageDTO);
+            console.log("route response: ",data);
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+              console.log("Axios error: ", error);
+            } else {
+              console.log("Another error: ", error);
+            }
+        }
+
+        
     }
     const defaultValue = {
         points,
